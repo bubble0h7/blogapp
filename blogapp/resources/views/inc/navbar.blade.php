75d58797e297bfea10e2005c0cceddf223926dbd
bubble0h7 <faydawnmorris@gmail.com>
2018-10-01 23:16:58 +1300

    Fixed logo directory, simplified navbar and fleshed out the About Me page

--- blogapp/resources/views/inc/navbar.blade.php
+++ blogapp/resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+{{-- <nav class="navbar navbar-default">
         <a class="navbar-brand" href="/">
           <img src="img/brand.png" width="30" height="30" alt="">
         </a>
@@ -7,7 +7,7 @@
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav justify-content-center mr-auto">
             <li class="nav-item active">
               <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -22,5 +22,37 @@
             </li>
           </ul>
           
+        </div>
+      </nav> --}}
+
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">
+          <img src="/img/brand.png" width="30" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarsExample02">
+          <ul class="navbar-nav mr-auto">
+            {{-- <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li> --}}
+            <li class="nav-item">
+                <a class="nav-link" href="/blog">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Posts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About Me</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" target="_blank" href="https://github.com/bubble0h7">Github</a>
+          </li>
+          </ul>
+          {{-- <form class="form-inline my-2 my-md-0">
+            <input class="form-control" type="text" placeholder="Search">
+          </form> --}}
         </div>
       </nav>
--- blogapp/resources/views/pages/about.blade.php
+++ blogapp/resources/views/pages/about.blade.php
@@ -3,7 +3,23 @@
 @section('content')
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-          <h1 class="display-4">About me</h1>
-          <p class="lead">Want to know a little bit about me? No..? Too bad.</p>
+          <h1 class="display-4">Hi. Hello.</h1>
+          <p class="lead">Want to know a little bit about me? No..? Well this is awkward.</p>
+    </div>
+</div>
+    <div class="container">
+        <p class="lead">The internet knows me as bubble0h7, but you can call me Fay.</p>
+        <p>
+            Does anyone ever know how to start these things? I sure don't. There we go - that's something, I guess.
+        </p>
+        <p>
+            <b>Perhaps a bit of background?</b> I have a bachelor's degree in Information and Communications Technology. I hate dressing up though and didn't bother attending graduation. So, does it even count? I graduated, but never actually graduated. Something something, if a trees falls in the forest and no one is around to hear it, something something; I'm pretty sure it's just like that. I have a fancy piece of paper with my name on it though, so that's cool.
+        </p>
+        <p>
+            <b>Defining traits?</b> Well, I'm female. An IT professional once told me that's important and that I should "sell" that about myself more, though I still fail to see why. How about something more substantial? I ask a million questions to anyone who will humor me enough to answer. Curiousity is the key to learning and boy, would life be boring if you stopped learning new things. What else...
+        </p>
+        <p>
+            <b>Why programming?</b> A lot of people consider themselves left-brained or right-brained. I'm split down the middle. I need a mix of creativity and logic to keep me happy. Programming gives me a platform to create gorgeous interfaces with slick functionality all the while solving problems.
+        </p>
     </div>
 @endsection
